f48759e13eb237f634baa9721b92ac1c90939590
Aidan Lee <aidanwlee@gmail.com>
2021-12-08 11:21:21 -0500

    Initial Commit

---
+++ AthleteAnalytics/AthleteAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthleteAnalyticsApp.swift
+//  AthleteAnalytics
+//
+//  Created by Aidan Lee on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AthleteAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
